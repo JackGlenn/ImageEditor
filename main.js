47c81eac5a150f949804af4f44127b69064bfd68
Jack <jglenn1038@gmail.com>
2024-07-05 15:40:48 -0400

    file upload to canvas

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-import './style.css'
+"use strict";
+
+import "./style.css";
 
 const canvas = document.querySelector("#canvas");
 const canvasContext = canvas.getContext("2d");
 
 canvasContext.fillStyle = "green";
 canvasContext.fillRect(10, 10, 150, 100);
+
+const fileInput = document.querySelector("#fileInput");
+console.log(fileInput.files.length);
+
+fileInput.addEventListener(
+    "change",
+    async () => {
+        const fileList = fileInput.files;
+        const imageBitmap = await createImageBitmap(fileList[0]);
+        canvasContext.drawImage(imageBitmap, 0, 0);
+    },
+    false
+);
+
